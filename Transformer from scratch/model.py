22cb85d6e5913aa2670557b50f167cf2c9c0aa86
Umair Khalil <98485634+umairkhalil189@users.noreply.github.com>
2025-05-31 22:55:01 +0500

    Update model.py

--- Transformer from scratch/model.py
+++ Transformer from scratch/model.py
@@ -124,20 +124,20 @@ class ResidualConnection(nn.Module):
        return x + self.dropout(sublayer(self.norm(x)))
    
 class EncoderBlock(nn.Module):
-   def __init__(self, self_attentionBlock: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock , dropout: float) -> None:
+   def __init__(self, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock , dropout: float) -> None:
       super().__init__()
       self.self_attention_block= self.self_attention_block
       self.feed_forward_block = feed_forward_block
-      self.residual_connections = nn.ModuleList([Residual_COnnection(dropout) for _in range(2)])
-
-   def forward(selff, x, src_mask):
+      self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+      
+   def forward(self, x, src_mask):
       x= self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
       x= self.residual_connections[1](x, lambda x: self.feed_forward_block)
       return x
 
 
-class Encoder(nn.Module)
-   def __init__(self, nn.ModuleList) -> None:
+class Encoder(nn.Module): 
+   def __init__(self, layers:  nn.ModuleList) -> None:
       super().__init__()
       self.layers= layers
       self.norm= LayerNormalization()
@@ -145,4 +145,21 @@ class Encoder(nn.Module)
    def forward(self, x, mask):
       for layer in layers:
          x= layer(x, mask)
-         return self.norm(x)
+      return self.norm(x)
+
+class DecodeBlock(nn.Module):
+   def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) ->None:
+      super().__init__()
+      self.self_attention_block = self_attention_block
+      self.cross_attention_block = cross_attention_block
+      self.feed_forward_block = feed_forward_block 
+      self.residual_connections = nn.Module([ResidualConnection(dropout) for _ in range(3)])
+
+      def forward(self, x, encoder_output, src_mask, tgt_mask):
+         x= self.residual_connections[0](x, lambda x: self_attention_block(x,x,x, tgt_mask))
+         x= self.residual_connections[1](x, lambda x: self_attention_block(x, encoder_output, encoder_output, src_mask))
+         x= self.residual_connections[2](x, self.feed_forward_block)
+         return x
+      
+class Decoder(nn.Module):
+   
