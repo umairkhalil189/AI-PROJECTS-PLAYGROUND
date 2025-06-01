92425b32c74e86a131fde73cfe72a750fd5e22dc
Umair Khalil <98485634+umairkhalil189@users.noreply.github.com>
2025-06-01 19:40:06 +0500

    Update model.py

--- Transformer from scratch/model.py
+++ Transformer from scratch/model.py
@@ -207,6 +207,48 @@ class Transfomer(nn.Module):
    
    def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int, tgt_seq_len: int, d_model: int=512, N: int=6, h:int=8, dropout: float= 0.1, d_ff= 2048 ) -> Transformer:
       src_embed= InputEmbeddings(d_model, src_vocab_size)
-      
+      tgt_embed = InputEmbeddings(d_model, tgt_vocab_size)
 
+      # Create the positional Encoding Layers
+      src_pos = PositionalEncoding(d_model, src_seq_len, dropout)
+      tgt_pos = PositionalEncoding(d_model, tgt_seq_len, dropout)
+
+      #Create the encoder blocks
+      encoder_blocks = []
+      for _ in range(N):
+         encoder_self_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+         feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+         encoder_block.append(encoder_block)
+
+      #Create the Decoder blocks
+
+      decoder_blocks = []
+      for _ in range(N):
+         decoder_self_attention_block = MultiHeadAttentionBlock(d_model , h, dropout)
+         decoder_cross_attention_block= MultiHeadAttentionBlock(d_model, h, dropout)
+         feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+         decoder_block = DecoderBlock(decoder_self_attention_block, decoder_cross_attention_block, feed_forward_block, dropout)
+         decoder_blocks.append(decoder_block)
+
+      #Create the encoder and the decoder
+
+      encoder = Encoder(nn.ModuleList(encoder_blocks))
+      decoder = Decoder(nn.ModuleList(decoder_block))
+
+      #Create the projection Layer
+      Projection_layer= ProjectionLayer(d_model, tgt_voacab_size)
+
+      #Create a Transformer
+
+      Transfomer = Transfomer(encoder, decoder, src_embed, tgt_embed, src_pos, tgt_pos, Projection_layer)
+
+      #Initialize the parameters
+
+      for p in transfomer.parameter():
+         if p.dim() > 1:
+            nn.init.xaivier_uniform(p)
+      
+      return transfomer
+   
+   
 
