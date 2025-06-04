80d43edc0adedbe5bcb055f2eadab8a8310e9a23
Umair Khalil <98485634+umairkhalil189@users.noreply.github.com>
2025-06-04 16:32:10 +0500

    Update dataset.py

--- Transformer from scratch/dataset.py
+++ Transformer from scratch/dataset.py
@@ -16,3 +16,11 @@ class BilingualDataset(Dataset):
         self.sos_token = torch.Tensor([tokenizer_src.token_to_id('[SOS]' )], dtype = torch.int64)
         self.eos_token = torch.Tensor([tokenizer_src.token_to_id('[EOS]' )], dtype = torch.int64)
         self.pad_token = torch.Tensor([tokenizer_src.token_to_id('[PAD]' )], dtype = torch.int64)
+
+    def __len__(self):
+        return len(self.ds)
+    
+    def __getitem__(self, index):
+        src_tgt_pair = self.ds[index]
+        src_text = src_tgt_pair['translation'][self.src_lang]
+        tgt_text = src_tgt_pair['translation'][self.tgt_lang]
