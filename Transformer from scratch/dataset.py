883be752de38355230c40ebe86141637e877813c
Umair Khalil <98485634+umairkhalil189@users.noreply.github.com>
2025-06-04 20:21:11 +0500

    Update dataset.py

--- Transformer from scratch/dataset.py
+++ Transformer from scratch/dataset.py
@@ -24,3 +24,48 @@ class BilingualDataset(Dataset):
         src_tgt_pair = self.ds[index]
         src_text = src_tgt_pair['translation'][self.src_lang]
         tgt_text = src_tgt_pair['translation'][self.tgt_lang]
+
+        enc_input_tokens = self.tokenizer_src.encode(src_text).ids
+        dec_input_tokens = self.tokenizer_tgt_encode(tgt_text).ids 
+
+        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2
+        dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) - 1
+
+        if enc_num_padding_token <0 or dec_input_len_token <0:
+            raise ValueError("sentence is too Long.")
+        
+        #Add SOS & EOS to the Source Text ! SOS= Start of sentence, EOS = End of sentence
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens, dtype = torch.int64),
+                self.eos_token, 
+                torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype= torch.int64)
+            ]
+        )
+        #Add SOS to decoder Input
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor([self.pad_token] * dec_num_padding_tokens , dtype= torch.int64)
+            ]
+        )
+
+        #Add EOS to the label 
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens, dtype = torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token]* dec_num_padding_tokens, dtype = torch.int64)
+            ]
+        )
+
+        assert encoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) = self.seq_len
+
+        return(
+            "encoder_input": encoder_input, #Seq_Len
+            "decoder_input": decoder_input, #Seq_Len
+
+        )
