c9a6961c8b7dc3111a6e9120e670b57e43f51074
Umair Khalil <98485634+umairkhalil189@users.noreply.github.com>
2025-06-10 17:55:14 +0500

    Update validate.py

--- Transformer from scratch/validate.py
+++ Transformer from scratch/validate.py
@@ -41,9 +41,17 @@ def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_
         out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
 
         #Get the next token
-        prob = model.project(out(:, -1))
+        prob = model.project(out[:, -1])
 
         #Select the token with the max probability (because it is a greedy search)
+        _, next_word =  torch.max(prob, dim=1)
+        decoder_input = torch.cat([decoder_input, torch.empty(1,1).type_as(source).fill_(next_word.item()).to(device)], dim=1)
+
+        if next_word == eos_idx:
+            break
+
+    return decoder_input.squeeze(0)
+
 
 
 
@@ -66,3 +74,26 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
             encoder_mask = batch['encoder_mask'].to(device)
 
             assert batch.size(0) == 1, "Batch Size must be 1 for validation"
+
+            model_out= greedy_decode(model, source, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
+
+            source_text = batch['src_text'][0]
+            target_text = batch['tgt_text'][0]
+            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+            source_texts.append(source_text)
+            expected.append(target_text)
+            predicted.append(model_out_text)
+
+            #Print to console
+
+            print_msg('-'*console_width)
+            print_msg(f'SOURCE: {source_text}')
+            print_msg(f'TARGET: {target_text}')
+            print_msg(f'PREDICTED: {model_out_text}')
+
+            if count == num_examples:
+                break
+
+            
+
