ee090fbb766cf6273f09e601196940065d5be1ab
Umair Khalil <98485634+umairkhalil189@users.noreply.github.com>
2025-08-12 14:58:36 +0500

    update

--- Color Detection using opencv/main.py
+++ Color Detection using opencv/main.py
@@ -3,7 +3,14 @@ from utils import get_limits
 from PIL import Image
 
 
-yellow= [0, 255, 255]
+# yellow= [0, 255, 255]
+colors_to_detect = {
+    "Yellow": [0, 255, 255],
+    "Red": [0, 0, 255],
+    "Blue": [255, 0, 0],
+    "Green": [0, 255, 0]
+}
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -11,20 +18,24 @@ while True:
 
     hsvimage= cv2.cvtColor(frame, cv2.COLOR_BGR2HSV )
    
-    lowerLimit, upperLimit = get_limits(color = yellow)
-    mask = cv2.inRange(hsvimage, lowerLimit, upperLimit  )
+    # lowerLimit, upperLimit = get_limits(color = yellow)
+    for color_name, bgr in colors_to_detect.items():
+        lowerLimit, upperLimit = get_limits(bgr)
+        mask = cv2.inRange(hsvimage, lowerLimit, upperLimit  )
 
-    mask_ = Image.fromarray(mask)
+        mask_ = Image.fromarray(mask)
 
-    bbox= mask_.getbbox()
+        bbox= mask_.getbbox()
 
 
-    if bbox is not None:
-        x1, y1, x2, y2 = bbox
-        frame = cv2.rectangle(frame, (x1, y1), (x2, y2) ,(0, 255, 0), 5)
-    else:
-        print("No Bounding Box found")
-    
+        if bbox is not None:
+            x1, y1, x2, y2 = bbox
+            frame = cv2.rectangle(frame, (x1, y1), (x2, y2) ,(0, 255, 0), 5)
+            cv2.putText(frame, color_name , (x1, y1 - 10), 
+                cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 255), 2)
+        else:
+            print("No Bounding Box found")
+
     print(f"Bounding Box is:    {bbox}")
     cv2.imshow('frame', frame)
     # cv2.imshow('frame', mask)
--- Color Detection using opencv/utils.py
+++ Color Detection using opencv/utils.py
@@ -1,10 +1,16 @@
 import numpy as np
 import cv2
 
+colors_to_detect = {
+    "Yellow": [0, 255, 255],
+    "Red": [0, 0, 255],
+    "Blue": [255, 0, 0],
+    "Green": [0, 255, 0]
+}
 
-def get_limits(color):
+def get_limits(color_bgr):
     
-    c= np.uint8([[color]])
+    c= np.uint8([[color_bgr]])
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
 
     lowerLimit = hsvC[0][0][0] -10, 100, 100
